52de3c0f08c7c81b122ddd240769f5ea2e8384ef
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-08-26 21:34:25 -0300

    .

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,6 +7,10 @@
     <title>Mater Educatrix - Portal Acadêmico</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <link rel="icon" href="/img/icone.png">
+
+    <link rel="manifest" href="{{ asset('manifest.json') }}" crossorigin="use-credentials">
 </head>
 
 <body class="bg-slate-950 text-slate-200 font-sans antialiased min-h-screen">
@@ -14,6 +18,16 @@
     <main class="min-h-screen">
         @yield('content')
     </main>
+
+    <script>
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js')
+                    .then(reg => console.log('Service Worker registrado com sucesso!', reg.scope))
+                    .catch(err => console.log('Falha ao registrar o Service Worker:', err));
+            });
+        }
+    </script>
 
 </body>
 
