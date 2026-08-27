cd8a5a64253e3dcea02c359e3267d4227ee1b58b
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-08-26 21:35:56 -0300

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -93,32 +93,31 @@
                                     </a>
                                 @endif
 
+                                <div x-data="{ showPassword: false }" class="relative w-full">
+                                    <input id="password" :type="showPassword ? 'text' : 'password'" name="password"
+                                        required
+                                        class="w-full h-12 pl-4 pr-12 rounded-xl bg-slate-950 border border-slate-800 text-white focus:outline-none focus:border-amber-500 transition">
+
+                                    <button type="button" @click="showPassword = !showPassword"
+                                        class="absolute right-4 top-1/2 -translate-y-1/2 text-slate-400 hover:text-amber-500 focus:outline-none transition">
+                                        <!-- Ícone do Olho Aberto (Senha Visível) -->
+                                        <svg x-show="showPassword" class="w-5 h-5" fill="none" stroke="currentColor"
+                                            viewBox="0 0 24 24" x-cloak>
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                        </svg>
+
+                                        <!-- Ícone do Olho Fechado (Senha Oculta) -->
+                                        <svg x-show="!showPassword" class="w-5 h-5" fill="none" stroke="currentColor"
+                                            viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858-5.908a10.018 10.018 0 014.122-.963c4.478 0 8.268 2.943 9.542 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21f-9-9m0 0L3 3" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
-
-                            <div x-data="{ showPassword: false }" class="relative w-full">
-                                <input id="password" :type="showPassword ? 'text' : 'password'" name="password" required
-                                    class="w-full h-12 pl-4 pr-12 rounded-xl bg-slate-950 border border-slate-800 text-white focus:outline-none focus:border-amber-500 transition">
-
-                                <button type="button" @click="showPassword = !showPassword"
-                                    class="absolute right-4 top-1/2 -translate-y-1/2 text-slate-400 hover:text-amber-500 focus:outline-none transition">
-                                    <!-- Ícone do Olho Aberto (Senha Visível) -->
-                                    <svg x-show="showPassword" class="w-5 h-5" fill="none" stroke="currentColor"
-                                        viewBox="0 0 24 24" x-cloak>
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
-
-                                    <!-- Ícone do Olho Fechado (Senha Oculta) -->
-                                    <svg x-show="!showPassword" class="w-5 h-5" fill="none" stroke="currentColor"
-                                        viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858-5.908a10.018 10.018 0 014.122-.963c4.478 0 8.268 2.943 9.542 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21f-9-9m0 0L3 3" />
-                                    </svg>
-                                </button>
-                            </div>
-
                         </div>
 
                         {{-- Remember --}}
