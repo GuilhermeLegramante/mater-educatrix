49fdf192b6119f209a37e5e0252bc89876acd693
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-09-02 13:22:11 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -29,20 +29,8 @@ Route::get('/manifest.json', function () {
     ]);
 });
 
-Route::get('/', function () {
-    if (auth()->user()->can('admin')) {
-        return redirect()->route('dashboard');
-    } else {
-        return redirect()->route('students.index');
-    }
-})->middleware(['auth'])->name('dashboard');
-
 Route::get('/dashboard', function () {
-    if (auth()->user()->can('admin')) {
-        return redirect()->route('dashboard');
-    } else {
-        return redirect()->route('students.index');
-    }
+    return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
